f8952a88c067feed60178eedb0a80e68679a7153
Britt Carter <brittbmh@gmail.com>
2019-02-13 14:24:59 -0600

    add new guest component and input fields

--- src/components/New Event/NewEventGuests.js
+++ src/components/New Event/NewEventGuests.js
@@ -2,10 +2,37 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class NewEventGuests extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            guest: '',
+            email: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = event.target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    addGuest = (event) => {
+        event.preventDefault();
+        this.props.dispatch({type: 'ADD_GUEST', payload: this.state })
+    }
     render() {
         return (
             <div>
-                test
+                <h3>Enter Guest Information</h3>
+                <input name="guest" type="text" placeholder="Guest Name" onChange={this.handleChange} />
+                <input name="email" type="text" placeholder="Guest Email" onChange={this.handleChange} />
+                <button onClick={this.addGuest}>Add Guest</button>
+                {JSON.stringify(this.state)}
             </div>
         )
     }
